c308de0da9345a1dda30b1471b411d0a4ddea02d
Leonardo dos Santos Dias <leonardo.dias@movemais.com>
2024-04-24 14:49:28 -0300

    feat: criando chessException e chessPosition

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import boardgame.Position;
+
 public class ChessPosition {
 	
 	private char column;
@@ -7,9 +9,9 @@ public class ChessPosition {
 	
 	public ChessPosition(char column, int row) {
 		
-//		if () {
-//			
-//		}
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+		}
 		this.column = column;
 		this.row = row;
 	}
@@ -18,15 +20,21 @@ public class ChessPosition {
 		return column;
 	}
 
-	public void setColumn(char column) {
-		this.column = column;
-	}
-
 	public int getRow() {
 		return row;
 	}
-
-	public void setRow(int row) {
-		this.row = row;
+	
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
 	}
+	
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+
 }
